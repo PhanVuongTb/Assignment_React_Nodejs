e55b6c72d79257d080c18ab0392c2392e5a45de2
Phan Vuong <pducvuongtb@gamil.com>
2022-04-13 20:27:29 +0700

    search server

--- Server/controllers/products.js
+++ Server/controllers/products.js
@@ -1,4 +1,3 @@
-import products from '../models/products'
 import Product from '../models/products'
 // const products = [
 //     {id:1,name:"Product 1"},
@@ -7,50 +6,59 @@ import Product from '../models/products'
 
 
 // list sản phẩm
-export const listProduct = async (request,response)=>{
-    try{
+export const listProduct = async (request, response) => {
+    try {
         const product = await Product.find().exec()
         response.json(product)
         // response.json(products)
-    }catch(error){
-        response.status(400).json({message:"Khong tim thay data"})
+    } catch (error) {
+        response.status(400).json({ message: "Khong tim thay data" })
     }
 }
 
 
-export const listProductDetail = async (request,response)=>{
-    const product = await products.findById({_id:request.params.id}).exec()
+export const listProductDetail = async (request, response) => {
+    const product = await Product.findById({ _id: request.params.id }).exec()
     response.json(product)
 }
 
 // thêm sản phẩm
-export const createProduct = async (request,response)=>{
-    try{
+export const createProduct = async (request, response) => {
+    try {
         const product = await Product((request.body)).save()
         response.json(product)
         // response.json(products)
-    }catch(error){
-        response.status(400).json({message:"Khong the tao moi"})
+    } catch (error) {
+        response.status(400).json({ message: "Khong the tao moi" })
     }
     // products.push(request.body)
     // response.json(products)
 }
 
 // xóa sản phẩm
-export const deleteProduct = async (request,response)=>{
-    try{
-        const product = await products.findOneAndDelete({_id:request.params.id}).exec()
-    response.json(product);
-    }catch(error){
-        response.status(400).json({message:"ko xóa dc"})
-}
-    
-}
-export const updateProduct = async (request,response)=>{
+export const deleteProduct = async (request, response) => {
     try {
-        const product = await products.findOneAndUpdate({_id:request.params.id}, request.body)
+        const product = await Product.findOneAndDelete({ _id: request.params.id }).exec()
         response.json(product);
     } catch (error) {
-        response.status(400).json({message:"ko sửa được"})
+        response.status(400).json({ message: "ko xóa dc" })
+    }
+
+}
+export const updateProduct = async (request, response) => {
+    try {
+        const product = await Product.findOneAndUpdate({ _id: request.params.id }, request.body)
+        response.json(product);
+    } catch (error) {
+        response.status(400).json({ message: "ko sửa được" })
+    }
+}
+
+export const searchProduct = async (req, res) => {
+    try {
+        const product = await Product.find({ 'name': new RegExp(req.params.name, 'i') }).exec();
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({ message: "Không thể tìm thấy Product" });
     }
 }
--- Server/routes/product.js
+++ Server/routes/product.js
@@ -1,12 +1,13 @@
 import express from "express"
-import { createProduct, deleteProduct, listProduct, listProductDetail, updateProduct } from "../controllers/products";
+import { createProduct, deleteProduct, listProduct, listProductDetail, updateProduct, searchProduct } from "../controllers/products";
 const router = express.Router();
 
 
-router.get('/products',listProduct)
-router.get('/products/:id',listProductDetail)
+router.get('/products', listProduct)
+router.get('/products/:id', listProductDetail)
 router.post('/products', createProduct)
-router.delete('/products/:id',deleteProduct)
-router.put('/products/:id',updateProduct)
+router.delete('/products/:id', deleteProduct)
+router.put('/products/:id', updateProduct)
+router.get('/sproduct/:name', searchProduct)
 
 export default router;
